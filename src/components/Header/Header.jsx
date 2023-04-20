cd4d83222cb87741d9f644c8c3af9ba5c5fe7774
Oleksii Horban <lehagorban93@gmail.com>
2023-04-20 18:56:16 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
+import React from "react";
 import Button from "../Button/Button";
-import {useTelegram} from "../../hooks/useTelegram";
-import './Header.css';
+import { useTelegram } from "../../hooks/useTelegram";
+import "./Header.css";
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+  const { user, onClose } = useTelegram();
 
-    return (
-        <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
-        </div>
-    );
+  return (
+    <div className={"header"}>
+      <Button onClick={onClose}>Закрити</Button>
+      <span className={"username"}>{user?.username}</span>
+    </div>
+  );
 };
 
 export default Header;
